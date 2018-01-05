121953a88b475e10354048f0cb5072afe5c801d1
Pawel Lorencowicz <p.lorencowicz@gmail.com>
2018-01-05 14:28:00 +0100

    try to change code to object and prototypes; not working yet

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ function addNewTask() {
     note.appendChild(addNote)
     note.appendChild(clearNote)
     main.appendChild(note)
+
     function addTaskToTasksList() {
         if (title.value !== '' && content.value !== '') {
             let taskList = localStorage.getItem('taskList') || '{}'
@@ -59,39 +60,74 @@ function viewList() {
         tasksListItems = JSON.parse(tasksListItems)
         for (let property in tasksListItems) {
             if (tasksListItems.hasOwnProperty(property)) {
-                new CreateNewTask(property, tasksListItems[property])
+                new Task(property, tasksListItems[property])
             }
         }
     }
 }
 
-function CreateNewTask(taskTitle, taskContent) {
-    let note = document.createElement('div')
-    let title = document.createElement('div')
-    let content = document.createElement('div')
-    let delNote = document.createElement('button')
-    note.className = 'note'
-    title.className = 'title'
-    content.className = 'content'
-    delNote.className = 'del'
-    note.appendChild(title)
-    note.appendChild(content)
-    note.appendChild(delNote)
-    title.innerText = taskTitle
-    content.innerText = taskContent
-    delNote.innerText = 'Delete'
-    delNote.addEventListener('click', removeTaskFromTasksList)
-    main.appendChild(note)
+// function CreateNewTask(taskTitle, taskContent) {
+//     let note = document.createElement('div')
+//     let title = document.createElement('div')
+//     let content = document.createElement('div')
+//     let delNote = document.createElement('button')
+//     note.className = 'note'
+//     title.className = 'title'
+//     content.className = 'content'
+//     delNote.className = 'del'
+//     note.appendChild(title)
+//     note.appendChild(content)
+//     note.appendChild(delNote)
+//     title.innerText = taskTitle
+//     content.innerText = taskContent
+//     delNote.innerText = 'Delete'
+//     delNote.addEventListener('click', removeTaskFromTasksList)
+//     main.appendChild(note)
 
-    function removeTaskFromTasksList() {
+//     function removeTaskFromTasksList() {
+//         let taskList = localStorage.getItem('taskList')
+//         taskList = JSON.parse(taskList)
+//         delete taskList[taskTitle]
+//         taskList = JSON.stringify(taskList)
+//         localStorage.setItem('taskList', taskList)
+//         main.removeChild(note)
+//         let alert = document.createElement('span')
+//         alert.innerText = 'Task removed from tasks list!\n'
+//         main.prepend(alert)
+//     }
+// }
+
+// not working :
+
+function Task(taskTitle, taskContent) {
+    this._taskTitle = taskTitle // ??????
+    this.element = document.createElement('div')
+    this.title = document.createElement('div')
+    this.content = document.createElement('div')
+    this.delNote = document.createElement('button')
+    this.element.className = 'note'
+    this.title.className = 'title'
+    this.content.className = 'content'
+    this.delNote.className = 'del'
+    this.element.appendChild(this.title)
+    this.element.appendChild(this.content)
+    this.element.appendChild(this.delNote)
+    this.title.innerText = taskTitle
+    this.content.innerText = taskContent
+    this.delNote.innerText = 'Delete'
+    this.delNote.addEventListener('click', this.removeFromList)
+    main.appendChild(this.element)
+}
+
+Task.prototype.removeFromList = function() {
         let taskList = localStorage.getItem('taskList')
         taskList = JSON.parse(taskList)
-        delete taskList[taskTitle]
+        delete taskList[this._taskTitle]
+        console.log(this._taskTitle)
         taskList = JSON.stringify(taskList)
         localStorage.setItem('taskList', taskList)
-        main.removeChild(note)
+        // main.removeChild(this.element)
         let alert = document.createElement('span')
         alert.innerText = 'Task removed from tasks list!\n'
         main.prepend(alert)
-    }
 }
